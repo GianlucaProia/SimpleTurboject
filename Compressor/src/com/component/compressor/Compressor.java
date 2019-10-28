e7563affea8e09fef06461e5ac5fa5008c264957
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-10-28 23:23:25 +0100

    ...

--- Compressor/src/com/component/compressor/Compressor.java
+++ Compressor/src/com/component/compressor/Compressor.java
@@ -20,6 +20,7 @@ public class Compressor implements CompressorInterface  {
 	private double beta;
 	
 	
+	
 	// Costruttore...
 	public Compressor(double P0in, double T0in, double Pout, double Tout ){
 		AirFlow airFlowIn = new AirFlow();
