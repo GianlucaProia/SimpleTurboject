bbafaf207ad434b80d313f43368f1c9ff9412334
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-11-03 17:52:38 +0100

    ...

--- CombustionCamber/src/com/component/combustioncamber/TestCEA.java
+++ CombustionCamber/src/com/component/combustioncamber/TestCEA.java
@@ -28,9 +28,44 @@ public class TestCEA {
 //	            "@echo Combustion|FCEA2");
 //	        builder.redirectErrorStream(true);
 //	        Process p = builder.start();
-			
-		 Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd C:/Users/Utente/Desktop/CEA && @echo Combustion|FCEA2 && exit\""); 
+		
+//		  Thread thread = new Thread(){
+//			    public void run(){
+//			   try {
+//				Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd C:/Users/Utente/Desktop/CEA && @echo Combustion|FCEA2 && exit\"");
+//			   } catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			    }
+//			  };
+//			  
+//			  Thread thread2 = new Thread(){
+//				    public void run(){
+//				   try {
+//					   Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd C:/Users/Utente/Desktop/CEA && @echo Combustion2|FCEA2 && exit\""); 
+//				   } catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				    }
+//				  };
+//		
+//				  thread.start();
+				  
+				  
+//		 Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd C:/Users/Utente/Desktop/CEA && @echo Combustion|FCEA2 && exit\""); 
+//		 
+//		 Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd C:/Users/Utente/Desktop/CEA && @echo Combustion2|FCEA2 && exit\""); 
+		
+		
+		RunMultyCea p1 = new RunMultyCea("cmd /c start cmd.exe /K \"cd C:/Users/Utente/Desktop/CEA && @echo Combustion|FCEA2 && @echo Combustion2|FCEA2 && @echo Combustion3|FCEA2 && exit\"");
+		//RunMultyCea p2 = new RunMultyCea("cmd /c start cmd.exe /K \"cd C:/Users/Utente/Desktop/CEA && @echo Combustion2|FCEA2 && exit\"");
+		p1.start();
+//		p2.start();
+
+		 
 	}
 	
-
+	
 }
