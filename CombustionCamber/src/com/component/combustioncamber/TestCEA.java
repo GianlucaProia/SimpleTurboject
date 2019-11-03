ba8682203c909ad8afdbe4b57e26d245d0b9eacc
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-11-03 17:54:14 +0100

    ...

--- CombustionCamber/src/com/component/combustioncamber/TestCEA.java
+++ CombustionCamber/src/com/component/combustioncamber/TestCEA.java
@@ -59,7 +59,12 @@ public class TestCEA {
 //		 Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd C:/Users/Utente/Desktop/CEA && @echo Combustion2|FCEA2 && exit\""); 
 		
 		
-		RunMultyCea p1 = new RunMultyCea("cmd /c start cmd.exe /K \"cd C:/Users/Utente/Desktop/CEA && @echo Combustion|FCEA2 && @echo Combustion2|FCEA2 && @echo Combustion3|FCEA2 && exit\"");
+		RunMultyCea p1 = new RunMultyCea(""
+				+ "cmd /c start cmd.exe /K \"cd C:/Users/Utente/Desktop/CEA"
+				+ "&& @echo Combustion|FCEA2"
+				+ "&& @echo Combustion2|FCEA2"
+				+ "&& @echo Combustion3|FCEA2"
+				+ "&& exit\"");
 		//RunMultyCea p2 = new RunMultyCea("cmd /c start cmd.exe /K \"cd C:/Users/Utente/Desktop/CEA && @echo Combustion2|FCEA2 && exit\"");
 		p1.start();
 //		p2.start();
