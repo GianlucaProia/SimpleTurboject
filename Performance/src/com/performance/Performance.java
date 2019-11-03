6eb52fd9c9a593c63271d519f5607016061c0443
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-11-03 15:19:27 +0100

    Spostato Performance nel progetto TurbojectEngine...

--- Performance/src/com/performance/Performance.java
+++
@@ -1,9 +0,0 @@
-package com.performance;
-
-public class Performance {
-
-	private double Trust;
-	private double Is;
-	
-	
-}
---
+++ TurbojectEngine/src/com/engine/performance/Performance.java
@@ -0,0 +1,5 @@
+package com.engine.performance;
+
+public class Performance {
+
+}
