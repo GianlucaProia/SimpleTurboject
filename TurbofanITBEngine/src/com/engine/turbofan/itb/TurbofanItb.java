cef7a7fa92a2989aa482b83286e0c1b53395f17e
Gianluca Proia <g.proia@tdnet.it>
2019-11-06 13:50:50 +0100

    Turbofan with ITG...

---
+++ TurbofanITBEngine/src/com/engine/turbofan/itb/TurbofanItb.java
@@ -0,0 +1,24 @@
+package com.engine.turbofan.itb;
+
+import com.component.combustioncamber.CombustionCamber;
+import com.component.compressor.Compressor;
+import com.component.fan.Fan.Fan;
+import com.component.inlet.Inlet;
+import com.component.nozzle.Nozzle;
+import com.component.turbine.Turbine;
+
+public class TurbofanItb {
+	
+	private Inlet inlet;
+	private Fan fan;
+	private Compressor lowCompressor;
+	private Compressor highCompressor;
+	private CombustionCamber mainCombustor;
+	private Turbine highTurbine;
+	private CombustionCamber itgCombustor;
+	private Turbine lowTurbine;
+	private Nozzle nozzle;
+	
+	
+
+}
