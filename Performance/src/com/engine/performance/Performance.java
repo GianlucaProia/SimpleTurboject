f39faecb2d4830a037872438855faf289bfa561c
Gianluca Proia <g.proia@tdnet.it>
2019-11-13 15:28:31 +0100

    Performance...

---
+++ Performance/src/com/engine/performance/Performance.java
@@ -0,0 +1,15 @@
+package com.engine.performance;
+
+public class Performance {
+
+	private double thrust;
+	private double specificThrust;
+	private double TSFC;
+	
+	private double etaTh;
+	private double etaProp;
+	private double etaTot;
+	
+	
+	
+}
