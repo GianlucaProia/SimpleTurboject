194ca5f402fc81118fe5e9177ebc2cc8ce3db0af
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-11-25 22:00:57 +0100

    ...

--- TurboFanJLinkProject/Turbofan/Components/CalculateOutput/Inlet/InletInstanceJavaObject.m
+++ TurboFanJLinkProject/Turbofan/Components/CalculateOutput/Inlet/InletInstanceJavaObject.m
@@ -10,7 +10,7 @@ Begin["`Private`"] (* Begin Private Context *)
  NewBasicInletObject[{Tin_,Pin_,gammaIn_,MachOut_},etad_]:=Module[{inletObject,airFlowIn,airFlowOut},
      inletObject=JavaNew["com.component.inlet.Inlet",etad];
      airFlowIn=JavaNew["com.air.flow.AirFlow",Tin,Pin,gammaIn];
-     airFlowOut=JavaNew["com.air.flow.AirFlow",MachOut];
+     airFlowOut=JavaNew["com.air.flow.AirFlow", MachOut];
      inletObject@setAirFlowIn[airFlowIn];
      inletObject@setAirFlowOut[airFlowOut];
      inletObject
