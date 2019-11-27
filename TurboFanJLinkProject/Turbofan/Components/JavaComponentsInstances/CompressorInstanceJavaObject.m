7e35e21bfa6c81fb3d97f277e0450efac6c77183
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-11-27 22:24:54 +0100

    ...

---
+++ TurboFanJLinkProject/Turbofan/Components/JavaComponentsInstances/CompressorInstanceJavaObject.m
@@ -0,0 +1,10 @@
+(* Wolfram Language Package *)
+
+BeginPackage["Turbofan`Components`JavaComponentsInstances`CompressorInstanceJavaObject`"]
+(* Exported symbols added here with SymbolName::usage *)  
+
+Begin["`Private`"] (* Begin Private Context *) 
+
+End[] (* End Private Context *)
+
+EndPackage[]
